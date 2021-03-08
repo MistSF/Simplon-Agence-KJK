d4f8fd82941ab16e2639b19b3011eb4a09f2ef28
SprinTech <jvansteenkiste2@gmail.com>
2021-03-08 16:18:02 +0100

    Test de code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import mysql.connector
 import config as cfg
 import actionDatabase as ad
-
 mydb = mysql.connector.connect(
     host=cfg.mysql["host"],
     user=cfg.mysql["user"],
@@ -11,8 +10,10 @@ mydb = mysql.connector.connect(
 
 run = True
 
+
 cursor = mydb.cursor(buffered=True)
 ad.createDatabase(cursor, "Agence_KJK")
+
 
 print("Welcome in agency data manager V1.0")
 
@@ -35,3 +36,21 @@ while run :
 
     elif entry == "show avg payment" :
         ad.getAvgNB(cursor, "Order_payments", 'payment_value')
+    elif entry == "get orders by state" :
+        ad.getOrdersBy(cursor, "state")
+    elif entry == "get orders by month" :
+        ad.getOrdersBy(cursor, "month")
+    elif entry == "sellers by state" :
+        ad.sellers_by_state(cursor)
+    elif entry == "average orders score" :
+        ad.average_orders_score(cursor)
+    elif entry == "orders by day" : #/!\
+        ad.orders_by_day(cursor)
+    elif entry == "order min price" :
+        ad.order_price(cursor, "MIN")
+    elif entry == "order max price" :
+        ad.order_price(cursor, "MAX")
+    elif entry == "average delivery time" :
+        ad.average_delivery_time(cursor)
+    elif entry == "get nb products by category" :
+        ad.getNbProductsByCategory(cursor)
